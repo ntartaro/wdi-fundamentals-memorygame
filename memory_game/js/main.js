1e271a43e209eef321c61714d6b5f401ee02f514
Nick Tartaro <ntartaro@gmail.com>
2018-08-18 00:18:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,16 +2,18 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];
+var cardOne = cards[0];
 var cardTwo = cards[2];
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+	}
 }
 
 console.log("User flipped " + cardOne);
