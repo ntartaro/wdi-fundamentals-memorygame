0653193b773843158b61e24c127c5bbbee735a3f
Nick Tartaro <ntartaro@gmail.com>
2018-08-19 09:55:14 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,33 +1,71 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-	} else {
-	alert("Sorry, try again.");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!")
+		} else {
+			alert("Sorry, try again.")
+		}
 	}
 }
 
-function flipcard(cardId) {
-
- if (cardsInPlay.length === 2) {
-	
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	} 
-
-	else {
-		alert("Sorry, try again.")
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipcard)
+    	document.getElementById('game-board').appendChild(cardElement)
 	}
+}
 
- }
-console.log("User flipped "+ cards[cardId])
-cardsInPlay.push(cards[cardId])
-checkForMatch()
+function createResetButton() {
+	document.getElementById('reset-button').addEventListener('click', reset)
+}
+
+function reset() {
+	document.getElementById('game-board').innerHTML = '';
+	cardsInPlay = []
+	createBoard()
+}
+
+function flipcard() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage)
+	
+	cardsInPlay.push(cards[cardId].rank)
+	checkForMatch()
 };
 
-flipcard(0);
-flipcard(2);
+
+createBoard()
+createResetButton()
